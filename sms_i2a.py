fd2e57648b113985ec1bb76914f30f8e7ec791a5
Ben Challenor <ben@challenor.org>
2013-02-03 00:09:14 +0000

    Fix for iOS6

--- sms_i2a.py
+++ sms_i2a.py
@@ -9,14 +9,14 @@ def readSMSdb(dbfile):
     sms = []
     conn = sqlite3.connect(dbfile)
     c = conn.cursor()
-    c.execute('SELECT address, date, text, flags FROM message where flags in (2,3)')
-    for sms_addr, sms_date, sms_body, sms_flag in c.fetchall():
+    c.execute('SELECT h.id as addr, m.date + 978307200 as date, m.text, is_from_me FROM message m INNER JOIN handle  h ON h.rowid = m.handle_id WHERE m.text IS NOT NULL')
+    for sms_addr, sms_date, sms_body, sms_is_from_me in c.fetchall():
         if sms_addr: sms_addr = sms_addr.encode('utf8')
         if sms_body:
             sms_body = sms_body.encode('utf8')
         else:
             sms_body = ""
-        sms.append((sms_addr, sms_date, sms_body, sms_flag))
+        sms.append((sms_addr, sms_date, sms_body, sms_is_from_me))
     return sms
 
 def output2File(data, output_file):
@@ -24,14 +24,10 @@ def output2File(data, output_file):
     f.write("""<?xml version='1.0' encoding='UTF-8' standalone='yes' ?>
     <?xml-stylesheet type="text/xsl" href="sms.xsl"?>
     <smses count="%s">""" % len(data))
-    for addr, date, body, flag in data:
-        if flag == 3:
-            flag = 2
-        elif flag == 2:
-            flag = 1
-        else:
-            flag = 1
-        f.write("""<sms protocol="0" address="%s" date="%d000" type="%s" subject="null" body="%s" toa="null" sc_toa="null" service_center="null" read="1" status="-1" locked="0" readable_date="" contact_name="(Unknown)" />\n""" % (addr, date,flag,body))
+    for addr, date, body, is_from_me in data:
+        # type 1 for received, 2 for sent
+        typ = 2 if is_from_me == 1 else 1
+        f.write("""<sms protocol="0" address="%s" date="%d000" type="%s" subject="null" body="%s" toa="null" sc_toa="null" service_center="null" read="1" status="-1" locked="0" readable_date="" contact_name="(Unknown)" />\n""" % (addr, date, typ, body))
     f.write("</smses>\n")
     f.close()
 
